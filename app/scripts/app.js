d2efbfd340f471fc16db8e922b9c10d97ca3d609
Mikael Vik <mikael.vik@gmail.com>
2012-11-28 12:20:25 +0100

    yeoman init angular

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var ninjaAngularApp = angular.module('ninjaAngularApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
